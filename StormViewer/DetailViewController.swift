b1f265f4d0b9de18d980fb1f306f4c8fe38979dc
Rajat16nov <rajat16nov@gmail.com>
2017-03-04 23:49:48 +0530

    Added social media sharing

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class DetailViewController: UIViewController {
 
@@ -16,6 +17,8 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = selectedImage
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .action, target: self,action: #selector(shareTapped))
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
         }
@@ -33,6 +36,20 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    func shareTapped() {
+//        let vc = UIActivityViewController(activityItems:
+//            [imageView.image!], applicationActivities: [])
+//        vc.popoverPresentationController?.barButtonItem =
+//            navigationItem.rightBarButtonItem
+//        present(vc, animated: true)
+        if let vc = SLComposeViewController(forServiceType:
+            SLServiceTypeFacebook) {
+            vc.setInitialText("Look at this great picture!")
+            vc.add(imageView.image!)
+            vc.add(URL(string: "http://www.photolib.noaa.gov/nssl"))
+            present(vc, animated: true)
+        }
     }
 
     /*
